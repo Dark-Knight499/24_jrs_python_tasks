02ec57e727f3cfa4e96ed6fcc4644027e17a6d57
Harssh <>
2024-07-06 00:20:27 +0530

    last question

--- maxlen.py
+++ maxlen.py
@@ -4,12 +4,16 @@ print("Enter words and to exit -1")
 while True:
     x = input("Enter a word : ")
     if x == "-1":
-        break
+            break
     words.append(x)
-    l.append(len(x))
-max = 0
-for i in range(len(l)):
-    if l[i] > l[max]:
-        max = i
-print(words[max])
-print(l[max])
+def count(l,words):
+    print("Enter words and to exit -1")
+    for i in words:
+        l.append(len(i))
+    max = 0
+    for i in range(len(l)):
+        if l[i] > l[max]:
+            max = i
+    return max
+max = count(l,words)
+print(words[max]," ",l[max])
